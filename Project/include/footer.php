ab68fc496904a6d74b4fcbf2f682b6617ca28cef
VuBinhGiaUy <71827306+VuBinhGiaUy@users.noreply.github.com>
2022-06-21 08:44:07 +0700

    Add files via upload

---
+++ Project/include/footer.php
@@ -0,0 +1,12 @@
+<footer class="bg-dark mt-5 py-5 text-white">
+    <div class="container pb-5 d-flex justify-content-between">
+      <h2>Project</h2>
+      <div class="social">
+        <img src="images/facebook.png" class="ml-1" width="40" height="40" alt="">
+        <img src="images/twitter.png" class="ml-1" width="40" height="40" alt="">
+        <img src="images/youtube.png" class="ml-1" width="40" height="40" alt="">
+        <img src="images/rss.png" class="ml-1" width="40" height="40" alt="">
+        <img src="images/share.png" class="ml-1" width="40" height="40" alt="">
+      </div>
+    </div>
+  </footer>
---
+++ Project/index.php
@@ -0,0 +1,19 @@
+<?php
+ include "include/head.php";
+?>
+    <div class="jumbotron front rounded-0">
+        <div class="container">
+           <h3>Banner</h3>
+        </div>
+    </div>
+    <div class="container">
+        <h3>Product Slider</h3>
+        <hr>
+        <div class="row">
+          <!-- loop through posts and ouput them here using col-md-4 and BS4 card -->
+        
+        </div>
+    </div>
+<?php
+ include "include/footer.php";
+?>
