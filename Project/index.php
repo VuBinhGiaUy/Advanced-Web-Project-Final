e65c21d0bf481c831a0c1e79bfc9dd442b0358ed
VuBinhGiaUy <giauy1309@gmail.com>
2022-06-22 09:25:27 +0700

    Add carousel

--- Project/index.php
+++ Project/index.php
@@ -9,10 +9,15 @@ include "include/head.php";
 <div class="container">
     <h3>Product Slider</h3>
     <br>
-    <div class="row">
-
+    <div class="carousel">
+        <div class="slideshow-container">
+            <!-- Loop here  -->
+        </div>
+        <div class="direction prev" tabindex="0" onclick="plusSlides(-1)" role="button">❮</div>
+        <div class="direction next" tabindex="0" onclick="plusSlides(1)" role="button">❯</div>
     </div>
 </div>
+<script src="main.js"></script>
 <?php
 include "include/footer.php";
 ?>
