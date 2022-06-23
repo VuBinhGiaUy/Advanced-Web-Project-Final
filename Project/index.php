7657ff10e23fe3e4a50137a0d6350b7de5e71064
HaiS <2059012@itec.hcmus.edu.vn>
2022-06-23 07:24:55 +0700

    add mainbanner

--- Project/index.php
+++ Project/index.php
@@ -1,9 +1,35 @@
 <?php
 include "include/head.php";
 ?>
-<div class="jumbotron front rounded-0">
-    <div class="container">
-        <h3>Banner</h3>
+<div class="jumbotron-fluid front rounded-0 mt-5">
+    <div class="row">
+        <img class="col front-image" src="https://www.elleman.vn/wp-content/uploads/2021/12/20/208672/review-phim-spider-man-no-way-home-elle-man-cover-1.jpeg" alt="">
+    </div>
+
+    <div class="row front-content">
+        <div class="col-3">
+            <img class="h-200 w-100" src="https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_.jpg" alt="">
+        </div>
+        <div class="col-9 align-self-end">
+            <div class="row">
+                <h4 class="col title-card card-outline text-uppercase">
+                    Spider-man:No way home
+                </h4>
+
+                <div class="w-100 d-md-none d-block"></div>
+
+                <h5 class="col col-md-2 mx-1 title-card text-uppercase">
+                    pg-13
+                </h5>
+
+                <h5 class="col col-md-2 mx-1 title-card text-uppercase">
+                    123min
+                </h5>
+            </div>
+            <div class="row d-none d-sm-block title-card">
+            gallery
+            </div>
+        </div>
     </div>
 </div>
 <div class="container">
