69f7963dc7acc729d5eb87daaf3bf5ab6513369d
HaiS <2059012@itec.hcmus.edu.vn>
2022-06-23 08:38:27 +0700

    mainbanner img and responsive fix

--- Project/index.php
+++ Project/index.php
@@ -3,30 +3,33 @@ include "include/head.php";
 ?>
 <div class="jumbotron-fluid front rounded-0 mt-5">
     <div class="row">
-        <img class="col front-image" src="https://www.elleman.vn/wp-content/uploads/2021/12/20/208672/review-phim-spider-man-no-way-home-elle-man-cover-1.jpeg" alt="">
+        <img class="front-image" src="https://www.elleman.vn/wp-content/uploads/2021/12/20/208672/review-phim-spider-man-no-way-home-elle-man-cover-1.jpeg" alt="">
     </div>
 
     <div class="row front-content">
-        <div class="col-3">
+        <div class="col-6 col-sm-3 offset-3 offset-sm-0 ">
             <img class="h-200 w-100" src="https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_.jpg" alt="">
         </div>
-        <div class="col-9 align-self-end">
+
+        <div class="w-100 d-sm-none d-block"></div>
+
+        <div class="col-10 col-sm-9 offset-1 offset-sm-0 align-self-end">
             <div class="row">
-                <h4 class="col title-card card-outline text-uppercase">
+                <h4 class="col title-card card-outline text-left text-uppercase">
                     Spider-man:No way home
                 </h4>
 
                 <div class="w-100 d-md-none d-block"></div>
 
-                <h5 class="col col-md-2 mx-1 title-card text-uppercase">
+                <h5 class="col col-md-2 mx-1 title-card card-outline text-center text-uppercase">
                     pg-13
                 </h5>
 
-                <h5 class="col col-md-2 mx-1 title-card text-uppercase">
+                <h5 class="col col-md-2 mx-1 title-card card-outline text-center text-uppercase">
                     123min
                 </h5>
             </div>
-            <div class="row d-none d-sm-block title-card">
+            <div class="row d-none d-md-block title-card">
             gallery
             </div>
         </div>
