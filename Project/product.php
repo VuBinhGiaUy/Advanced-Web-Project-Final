1d9d536131fd1098907db3e2b614979eba2a4e53
HaiS <2059012@itec.hcmus.edu.vn>
2022-06-25 00:17:57 +0700

    adding fliter and product box

--- Project/product.php
+++ Project/product.php
@@ -8,6 +8,53 @@ include 'include/head.php'
     </div>
 </div>
 
+<div class="container">
+    <div class="row">
+        <aside class="col-3 bg-dark text-white">
+            fliter
+        </aside>
+        <div class="col-9">
+            <div class="d-flex flex-wrap justify-content-between mx-3">
+                <div class="card mb-3">
+                    <img class="card-img-top" src="asset/MORBIUS.jpg" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">Morbius</h4>
+                        <p class="card-text">rating</p>
+                        <div class="d-flex justify-content-between">
+                            <p class="card-text">price</p>
+                            <button type="button" name="" id="" class="btn btn-warning btn-md">Buy</button>
+                        </div>
+                        <button type="button" name="" id="" class="btn btn-primary btn-md btn-block mt-3">Add to Cart</button>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <img class="card-img-top" src="asset/MORBIUS.jpg" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">Morbius</h4>
+                        <p class="card-text">rating</p>
+                        <div class="d-flex justify-content-between">
+                            <p class="card-text">price</p>
+                            <button type="button" name="" id="" class="btn btn-warning btn-md">Buy</button>
+                        </div>
+                        <button type="button" name="" id="" class="btn btn-primary btn-md btn-block mt-3">Add to Cart</button>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <img class="card-img-top" src="asset/MORBIUS.jpg" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">Morbius</h4>
+                        <p class="card-text">rating</p>
+                        <div class="d-flex justify-content-between">
+                            <p class="card-text">price</p>
+                            <button type="button" name="" id="" class="btn btn-warning btn-md">Buy</button>
+                        </div>
+                        <button type="button" name="" id="" class="btn btn-primary btn-md btn-block mt-3">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
 include 'include/footer.php'
 ?>
