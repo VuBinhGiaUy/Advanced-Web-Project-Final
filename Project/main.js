0fc1d8e74e4ff9d4bf1bc39ec4e1f5335af34135
VuBinhGiaUy <giauy1309@gmail.com>
2022-06-26 22:14:47 +0700

    Slick update

--- Project/main.js
+++ Project/main.js
@@ -2,13 +2,20 @@ $('.center-slide').slick({
   infinite: true,
   slidesToShow: 5,
   slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 2000,
 });
+
+// $('.center-slide').slick({
+//   nextArrow: '<div class="col-1" style="z-index: 10;"><i class="fas fa-chevron-circle-left next next-icon"></i></div>',
+//   prevArrow: '<div class="col-1" style="z-index: 10;"><i class="fas fa-chevron-circle-left prev prev-icon"></i></div>'
+// });
 
 let cartBtn = document.querySelector('.cart-btn');
 let cartWrapper = document.querySelector('.cart-wrapper');
-cartBtn.addEventListener("click", ()=> cartWrapper.classList.toggle("cart-on"));
+cartBtn.addEventListener("click", () => cartWrapper.classList.toggle("cart-on"));
 function init() {
-  if(!localStorage.hasOwnProperty("cart")) {
+  if (!localStorage.hasOwnProperty("cart")) {
     let cart = [];
     localStorage.setItem("cart", JSON.stringify(cart));
     console.log(localStorage.getItem("cart"));
@@ -17,9 +24,9 @@ function init() {
 
 function addBtnGroupEventListener() {
   let productCounter = document.querySelectorAll('.add-group-btn');
-  productCounter.forEach(function(counter) {
-    counter.addEventListener("click", function(event) {
-        addToCart(event.target);
+  productCounter.forEach(function (counter) {
+    counter.addEventListener("click", function (event) {
+      addToCart(event.target);
     })
   })
 }
@@ -33,19 +40,19 @@ function addToCart(el) {
     "price": el.getAttribute("data-price"),
   }
   let cart = [];
-  if(localStorage.getItem("cart").length > 0) {
+  if (localStorage.getItem("cart").length > 0) {
     cart = JSON.parse(localStorage.getItem('cart'));
   }
   let count = product.count
   let existingItem = false;
   cart.forEach((item) => {
-    if(item.id == product.id) {
+    if (item.id == product.id) {
       item.count++;
       count = item.count;
       existingItem = true;
     }
   })
-  if(!existingItem) {
+  if (!existingItem) {
     cart.push(product);
   }
   // update card on page
@@ -57,7 +64,7 @@ function addToCart(el) {
 function updateCart(cart) {
   let cartBtn = document.querySelector('.cart-btn .badge');
   cartBtn.innerText = cart.length;
-  if(cart.length > 0) {
+  if (cart.length > 0) {
     cart.forEach((item) => {
       let itemBadge = document.querySelector(`.product-${item.id} .badge`);
       let itemAlert = document.querySelector(`.product-${item.id} .bought`);
@@ -71,8 +78,8 @@ function updateCart(cart) {
   let total = 0;
   cart.forEach((item, i) => {
     total += (item.price * item.count);
-    output+= `<tr>
-      <th scope="row">${i +1}</th>
+    output += `<tr>
+      <th scope="row">${i + 1}</th>
       <td>${item.title}</td>
       <td>${item.price}</td>
       <td>${item.count} <i class="fa fa-trash"></i></td>
