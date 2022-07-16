483f703b59420d9e5922e7e8b97640dc6e1b91ef
VuBinhGiaUy <giauy1309@gmail.com>
2022-07-16 18:37:19 +0700

    Comment minor fix

--- Project/class/Comment.php
+++ Project/class/Comment.php
@@ -12,12 +12,21 @@ class Comment {
     }
 
 
-    public function fetchComments($post_id) {
-       
+    public function fetchComments($id) {
+       $sql = "SELECT review.* FROM review WHERE review.movie_id = ?";
+       $stmt = $this->conn->prepare($sql);
+       $stmt->bind_param("s", $id);
+       $stmt->execute();
+       $result = $stmt->get_result();
+       return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getComments() {
-        
+
+    public function insertComment($id,$like,$dislike,$content,$rating){
+        $sql = "INSERT INTO review VALUES (?,?,?,?,?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("iiisi",$id,$like,$dislike,$content,$rating);
+        $stmt->execute();
     }
 }
 
